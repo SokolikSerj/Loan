793c355cf05080059a28b90a9a7266e9098f9d5c
SokolikSerj <sokolikserg@gmail.com>
2023-01-20 11:19:57 +0200

    someth

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,7 @@ var __webpack_exports__ = {};
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-
+window.addEventListener('DOMContentLoaded', () => {});
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map
